9f566a79d88f5af61673cb7fb20abb5e86138560
Delfina Moncayo Account <delfina.moncayo@gmail.com>
2020-03-14 02:36:51 -0400

    populate five day forecast

--- js/scripts.js
+++ js/scripts.js
@@ -19,26 +19,6 @@ function banner() {
 
 }
 
-const updateWindChill = () => {
-    //calculate windchill 
-
-    const tempNumber = parseFloat (document.getElementById("temp").textContent);
-    const speedNumber = parseFloat (document.getElementById("speed").textContent);
-
-    let windchill = 35.74 + (0.6215 * tempNumber) - (35.75 * Math.pow(speedNumber, 0.16)) 
-    + (0.4275 * tempNumber * Math.pow(speedNumber, 0.16));
-    windchill = Math.round(windchill);
-
-
-    if(tempNumber<= 50 && speedNumber > 3) {
-        document.getElementById("chill").innerHTML = windchill + "&deg;";
-    } else {
-        document.getElementById("chill").textContent = "No wind chill";
-
-    }
-}
-
-
 //toggle sandwiche menu
 function toggleMenu () {    
     document.getElementById("primaryNav").classList.toggle("hide"); 
@@ -63,49 +43,43 @@ function adjustRating() {
     document.getElementById("ratingvalue").innerHTML = document.getElementById("rating").value;
 }
 
-// store the resource URL of the JSON
-const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+let populateTowns = () => {
+    // store the resource URL of the JSON
+    const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
-//use fetch() method to feed the required argument. First .then, returns a Promise. Second .then to work
-//with the converted response data in JS
-fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then( function (jsonObject) {
-    // console.table(jsonObject);   ----checking for valid response and data parsing---
-    //store the results of the converted response into an array
-const towns = jsonObject['towns'];
-    //To produce the output. We create a loop
-    for (let i = 0; i < towns.length; i++ ) {
-      if (towns[i].name === "Preston" || towns[i].name === "Fish Haven" || towns[i].name === "Soda Springs") {
-        let card = document.createElement('section');
-        let h2 = document.createElement('h2');      
-        h2.textContent = towns[i].name; 
-        card.appendChild(h2); 
-        let h3 = document.createElement('h3');
-        h3.textContent = towns[i].motto;
-        card.appendChild(h3);
-        let p = document.createElement('p');      
-        p.innerHTML = 'Current Population: ' + towns[i].currentPopulation + '<br/><br/>Year Founded: ' 
-        + towns[i].yearFounded + '<br/><br/>Average Rainfall: ' + towns[i].averageRainfall;
-        card.appendChild(p);                                             
-        let image = document.createElement('img');   
-        image.setAttribute('src', 'images/' + towns[i].photo); 
-        image.setAttribute('alt', towns[i].name); 
-        card.appendChild(image);
+    //use fetch() method to feed the required argument. First .then, returns a Promise. Second .then to work
+    //with the converted response data in JS
+    fetch(requestURL)
+      .then(function (response) {
+        return response.json();
+      })
+      .then( function (jsonObject) {
+        // console.table(jsonObject);   ----checking for valid response and data parsing---
+        //store the results of the converted response into an array
+    const towns = jsonObject['towns'];
+        //To produce the output. We create a loop
+        for (let i = 0; i < towns.length; i++ ) {
+          if (towns[i].name === "Preston" || towns[i].name === "Fish Haven" || towns[i].name === "Soda Springs") {
+            let card = document.createElement('section');
+            let h2 = document.createElement('h2');      
+            h2.textContent = towns[i].name; 
+            card.appendChild(h2); 
+            let h3 = document.createElement('h3');
+            h3.textContent = towns[i].motto;
+            card.appendChild(h3);
+            let p = document.createElement('p');      
+            p.innerHTML = 'Current Population: ' + towns[i].currentPopulation + '<br/><br/>Year Founded: ' 
+            + towns[i].yearFounded + '<br/><br/>Average Rainfall: ' + towns[i].averageRainfall;
+            card.appendChild(p);                                             
+            let image = document.createElement('img');   
+            image.setAttribute('src', 'images/' + towns[i].photo); 
+            image.setAttribute('alt', towns[i].name); 
+            card.appendChild(image);
 
-        document.querySelector('div.townsInfo').appendChild(card);
-    /*
-      "name": "Franklin",
-      "photo": "franklin.jpg",
-      "motto": "Where you will grow!",
-      "yearFounded": 1788,
-      "currentPopulation": 30458,
-      "averageRainfall": 21,
-     
-    */
-      }
-    }
-  })
-  .catch( error => console.log(error) );
+            document.querySelector('.townsInfo').appendChild(card);
+        
+          }
+        }
+      })
+      .catch( error => console.log(error) );
+}
